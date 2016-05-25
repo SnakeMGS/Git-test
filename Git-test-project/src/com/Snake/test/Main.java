3268f6aa0a03f7b2d0c3914db264be16b5684252
sony <sony@sony-PC.mshome.net>
2016-05-25 16:35:50 +0300

    add !!

--- Git-test-project/src/com/Snake/test/Main.java
+++ Git-test-project/src/com/Snake/test/Main.java
@@ -3,7 +3,7 @@ package com.Snake.test;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from Git Hub!");
+		System.out.println("Hello from Git Hub!!!");
 
 	}
 
